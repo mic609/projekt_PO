065d9e16c12ef794eb59b559ec08e6edff920da2
Jakub Rusek <53147009+jakubrusek@users.noreply.github.com>
2021-05-20 10:00:14 +0200

    classes 1.0

---
+++ Code/human.py
@@ -0,0 +1,43 @@
+from movableObject import MovableObject
+
+HEALTH_CAP = 100
+
+
+class Human(MovableObject):
+
+    amount = 0
+
+    # -------------------------------------------------------------------------
+    def __init__(self, x_coordinate, y_coordinate, age, health_points=HEALTH_CAP, resp=False, immunity=False,
+                 infected=False):
+        super().__init__(x_coordinate, y_coordinate)
+        self.age = age
+        self.health_points = health_points
+        self.resp = resp
+        self.immunity = immunity
+        self.infected = False
+        Human.amount += 1
+
+    # -------------------------------------------------------------------------
+    def reduce_health_bar(self, n):
+        new_hp = self.health_points - n
+        self.health_points = new_hp if new_hp >= 0 else 0
+
+    # -------------------------------------------------------------------------
+    def restore_health(self, n):
+        new_hp = self.health_points + n
+        self.health_points = new_hp if new_hp <= HEALTH_CAP else HEALTH_CAP
+
+    # -------------------------------------------------------------------------
+    def full_restore_health(self):
+        self.health_points = HEALTH_CAP
+
+    # -------------------------------------------------------------------------
+    def __str__(self):
+        return f"Human(x:{self.x_coordinate}," \
+               f" y:{self.y_coordinate}," \
+               f" age:{self.age}," \
+               f" hp:{self.health_points}," \
+               f" resp:{self.resp}," \
+               f" immunity:{self.immunity}," \
+               f" infected:{self.infected})"
---
+++ Code/movableObject.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+import random
+
+
+class MovableObject(ABC):
+
+    # -------------------------------------------------------------------------
+    def __init__(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+
+    # -------------------------------------------------------------------------
+    def move(self, x, y):
+        self.x_coordinate = x
+        self.y_coordinate = y
+
